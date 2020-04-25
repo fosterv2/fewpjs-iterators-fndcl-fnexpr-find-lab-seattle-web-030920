c22fcccb442405b817259b8b4379845b1c376d8a
Valerie Foster <fosterv2@outlook.com>
2020-04-25 01:04:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function testFunc() {
 
 function superbowlWin(records) {
   let record = records.find(record => record.result === "W")
-  if !!record ? (return record.year) : (return false)
+  !!record ? (return record.year) : (return false)
 }
